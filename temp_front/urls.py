1ce2d3e31d18ca269c33ffe1acdea683a1b2d992
Ali Yamini <mail@aliyamini.ir>
2020-05-13 23:18:51 +0430

    setting up sign up page

--- temp_front/urls.py
+++ temp_front/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('log-in', views.login, name='login'),
     path('log-out', views.mylogout, name='logout'),
+    path('sign-up', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- temp_front/views.py
+++ temp_front/views.py
@@ -15,6 +15,15 @@ def login(request):
         return redirect('/dashboard')
     return render(request, 'temp_front/log-in.html')
 
+def signup(request):
+    try:
+        request.user.auth_token.delete()
+    except (AttributeError, ObjectDoesNotExist):
+        pass
+    logout(request)
+    return render(request, 'temp_front/sign-up.html')
+
+
 @login_required(login_url="/log-in")
 def dashboard(request):
     fullname = request.user.get_full_name().title()

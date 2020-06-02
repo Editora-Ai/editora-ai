e6a07e09522083e9c530c1f76ec0feacc06df4bc
Ali Yamini <d.aliyamini@gmail.com>
2020-06-02 12:07:58 +0430

    adding .com urls.

--- editora_service/settings.py
+++ editora_service/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '94.182.189.110', 'editoraai.ir', 'www.editoraai.ir']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '94.182.189.110', 'editoraai.ir', 'www.editoraai.ir', 'editoraai.com', 'www.editoraai.com']]
 
 
 # Application definition

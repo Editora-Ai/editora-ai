de4755d64890a19e5dfd38b8595473f7d8bcb9ac
Ali Yamini <d.aliyamini@gmail.com>
2020-06-07 12:11:02 +0430

    adding endpoints for face removal.

--- editora_api/urls.py
+++ editora_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import ListBGR, DetailBGR
+from .views import ListBGR, DetailBGR, ListFR, DetailFR
 
 
 urlpatterns = [
     path('bgr/', ListBGR.as_view()),
     path('bgr/<int:pk>/', DetailBGR.as_view()),
+    path('fr/', ListFR.as_view()),
+    path('fr/<int:pk>/', DetailFR.as_view()),
 ]

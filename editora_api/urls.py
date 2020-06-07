739bf813ba301dcdd19c7911dd2b4f05650a064a
Ali Yamini <d.aliyamini@gmail.com>
2020-06-07 12:11:53 +0430

    ammend

--- editora_api/urls.py
+++ editora_api/urls.py
@@ -3,8 +3,10 @@ from .views import ListBGR, DetailBGR, ListFR, DetailFR
 
 
 urlpatterns = [
+    # Background removal endpoints
     path('bgr/', ListBGR.as_view()),
     path('bgr/<int:pk>/', DetailBGR.as_view()),
+    # Face removal endpoints
     path('fr/', ListFR.as_view()),
     path('fr/<int:pk>/', DetailFR.as_view()),
 ]

6a839e8121f664e4eefb1bbb542699454140b70b
Ali Yamini <d.aliyamini@gmail.com>
2020-06-09 21:56:42 +0430

    fixing email confirmation.

--- editora_service/urls.py
+++ editora_service/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import RedirectView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from allauth.account.views import confirm_email
 
 
 admin.autodiscover()
@@ -48,8 +49,8 @@ urlpatterns = [
     path('rest-auth/registration/',
          include('rest_auth.registration.urls')),
     url(r'^account/', include('allauth.urls')),
-
-
+    url(r"^verifyEmail/(?P<key>\w+)/$", confirm_email, name="my_confirm_email"),
+    
     # Editoria Service Documentation
    url(r'^api-docs(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    url(r'^api-docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),

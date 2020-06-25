32addd511bc2f33b403619741be53df9c2689508
Ali Yamini <d.aliyamini@gmail.com>
2020-06-25 22:10:07 +0430

    adding serializer

--- editora_api/serializers.py
+++ editora_api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers, generics
 from rest_auth.registration.serializers import RegisterSerializer
 from rest_auth.registration.views import RegisterView
-from .models import BGR, FR
+from .models import BGR, FR, PR
 
 
 class AdminBGRSerializer(serializers.ModelSerializer):
@@ -40,3 +40,19 @@ class UserFRSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'status', 'owner', 'img_id' , 'date_created', 'modified_image')
 
 
+class AdminPRSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PR
+        fields = ('id', 'img_id', 'status', 'owner', 'original_image', 'modified_image',
+                  'date_created')
+        read_only_fields = ('id', 'owner', 'img_id', 'date_created', 'modified_image')
+
+
+class UserPRSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PR
+        fields = ('id', 'status', 'original_image', 'modified_image',
+                  'date_created')
+        read_only_fields = ('id', 'status', 'owner', 'img_id' , 'date_created', 'modified_image')

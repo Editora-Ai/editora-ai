1fc9f16f94387f64aa70a8a26d267c6db3e8f2a8
Ali Yamini <d.aliyamini@gmail.com>
2020-06-25 21:40:43 +0430

    adding model for pr.

--- editora_api/models.py
+++ editora_api/models.py
@@ -75,3 +75,38 @@ class FR(models.Model):
 
     def __str__(self):
         return (str(self.date_created) + " | " + str(self.owner.firstname) + " " + str(self.owner.lastname) + " | " +  str(os.path.basename(str(self.original_image))))  + " | " + str(self.status)
+
+
+class PR(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, unique=False, on_delete=models.CASCADE)
+    original_image = models.ImageField(upload_to='pr/original')
+    modified_image = models.ImageField(upload_to='pr/modified', null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    img_id = models.CharField(max_length=100)
+
+    # Status field
+    QUEUED = 'queued'
+    SUCCESS = 'success'
+    FAILED = 'failed'
+    PROCESSING = 'processing'
+
+    STATUS = [
+        (QUEUED, 'Queued'),
+        (SUCCESS, 'Success'),
+        (FAILED, 'Failed'),
+        (PROCESSING, 'Processing'),
+    ]
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS,
+        default=QUEUED,
+    )
+
+    def original_filename(self):
+        return ntpath.basename(str(self.original_image))
+
+    def modified_filename(self, path):
+        return ntpath.basename(self.modified_image)
+
+    def __str__(self):
+        return (str(self.date_created) + " | " + str(self.owner.firstname) + " " + str(self.owner.lastname) + " | " +  str(os.path.basename(str(self.original_image))))  + " | " + str(self.status)
